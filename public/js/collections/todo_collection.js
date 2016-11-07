134466a72d5d6b21176b2cf892a068d084a9495b
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2016-11-06 22:42:18 -0700

    Add comment.

--- public/js/collections/todo_collection.js
+++ public/js/collections/todo_collection.js
@@ -3,6 +3,7 @@ var ToDoModel = require('../models/todo_model.js');
 
 var ToDoCollection = Backbone.Collection.extend({
   comparator: function(a, b) {
+    // Sort models in descending ID order, i.e. newest first.
     return b.id - a.id;
   },
   model: ToDoModel,
